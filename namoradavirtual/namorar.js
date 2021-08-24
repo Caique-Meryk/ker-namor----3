d260e8392a6d6311ed01627020bc215a9aa642cb
Caique-Meryk <85769715+Caique-Meryk@users.noreply.github.com>
2021-08-24 10:57:03 -0300

    Add files via upload

---
+++ namoradavirtual/namorar.js
@@ -0,0 +1,16 @@
+function sim(){
+    var image = document.createElement("img")
+    image.setAttribute("src", "images/euteamo.png")
+    image.setAttribute("width", "1000");
+    image.setAttribute("height", "400");
+    document.body.innerHTML = "";
+    document.body.appendChild(image)    
+}
+function nao(){
+    var image = document.createElement("img")
+    image.setAttribute("src", "images/tite.png")
+    image.setAttribute("width", "800");
+    image.setAttribute("height", "400");
+    document.body.innerHTML = "";
+    document.body.appendChild(image)  
+}
